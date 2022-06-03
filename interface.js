e8c18b770270c94baf04d0d8dde90cd75da9c249
Diego Oliveira <103788470+diegoolivei@users.noreply.github.com>
2022-06-02 21:02:52 -0300

    Jogo da velha
    Projeto desenvolvido para treino em Javascript

---
+++ interface.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+
+let squares = document.querySelectorAll(".square");
+
+squares.forEach((square)=>{
+    square.addEventListener('click', handleClick);
+})
+})
+
+
+function handleClick(event){
+
+    let square = event.target;
+    let position = square.id;
+
+    if(handleMove(position)){
+        
+        setTimeout(()=>{
+            alert("O Jogo Acabou! O Vencedor foi " + playerTime);
+        },10);
+    };
+
+  
+    updateSquares();
+
+}
+
+function updateSquares(){
+
+    let squares = document.querySelectorAll(".square");
+
+squares.forEach((square)=>{
+    let position = square.id;
+   let symbol = board[position];
+
+   if(symbol != ''){
+       square.innerHTML = `<div class='${symbol}'></div>`
+   }
+})
+
+let btn = document.getElementById("btn")
+
+btn.addEventListener('click', restart)
+
+function restart(){
+    let squares = document.querySelectorAll(".square")
+
+    squares.forEach(square => {
+        square.innerHTML = ""
+    }) 
+
+    board = ['', '', '', '', '', '', '', '', '']
+    gameOver = false
+}
+
+}
